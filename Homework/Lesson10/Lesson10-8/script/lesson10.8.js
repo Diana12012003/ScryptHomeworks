d406f972d23cfad56d2271ffbe17d6010677a2bf
Diana12012003 <dianusy777@gmail.com>
2025-05-21 13:08:55 +0300

    added directory Lesson10 and html and js files

--- Homework/Lesson10/Lesson10-8/script/lesson10.8.js
+++ Homework/Lesson10/Lesson10-8/script/lesson10.8.js
@@ -1,13 +1,13 @@
 const tableContainer = document.getElementById('tableContainer');
-const tableGeneratorForm = document.forms['table']; // Отримуємо форму
+const tableGeneratorForm = document.forms['table'];
 
 tableGeneratorForm.onsubmit = function (e) {
     e.preventDefault();
     tableContainer.innerText = '';
 
-    const linesValue = +tableGeneratorForm.lines.value; // Отримуємо кількість рядків
-    const cellsValue = +tableGeneratorForm.cells.value; // Отримуємо кількість колонок
-    const contentValue = tableGeneratorForm.content.value; // Отримуємо вміст
+    const linesValue = +tableGeneratorForm.lines.value;
+    const cellsValue = +tableGeneratorForm.cells.value;
+    const contentValue = tableGeneratorForm.content.value;
     console.log(linesValue, cellsValue, contentValue);
 
     for (let i = 0; i < linesValue; i++) {
@@ -17,10 +17,7 @@ tableGeneratorForm.onsubmit = function (e) {
             td.innerText = contentValue;
             tr.appendChild(td);
         }
-
         tableContainer.appendChild(tr);
-
     }
-
 };
 
